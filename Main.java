199d54111805dab597a401d7dcbeaea85d75c080
Shubham Vast <shubhamvast5@gmail.com>
2025-01-01 17:12:35 +0530

    Added commit message

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Working with Java code");
         System.out.println("Cool....!");
+        System.out.println("Our new Commits also considers");
     }
 }
